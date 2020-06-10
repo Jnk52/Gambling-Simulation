89ccc47d48293148ad5e7125e36c9d03481f960e
jnk52 <ajienkyanikam52@gmail.com>
2020-06-09 21:03:03 -0700

    UC3

--- Gambling.sh
+++ Gambling.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
-RandomCheck=$((RANDOM%2))
 WIN=1
-if [ $RandomCheck -eq $WIN ]
-then
-        echo Gambler Wins
-else
-        echo Gambler looses
-fi
+STAKE=100
+BET=1
+MAX_PROFIT_PER_DAY=50
+MAX_LOSS_PER_DAY=-50
+pocket=0
+while [[ $pocket -lt $MAX_PROFIT_PER_DAY && $pocket -gt $MAX_LOSS_PER_DAY ]]
+do
+        RandomCheck=$((RANDOM%2))
+        if [ $WIN -eq $betCheck ]
+        then
+                pocket=$(($pocket+$BET))
+        else
+                pocket=$(($pocket-$BET))
+        fi
+done
+
+echo "$pocket"
+echo "pocket ammout =$(($STAKE+$pocket))"
 
